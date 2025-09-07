2a1dd50b39b7ee919838912a8581b651ff5dbaab
Safal-Singhh <safalsingh999@gmail.com>
2025-09-07 23:50:58 +0530

    updated new file

--- Arrays/Sliding Window/713. Subarray Product Less Than K.cpp
+++ Arrays/Sliding Window/713. Subarray Product Less Than K.cpp
@@ -14,3 +14,30 @@
 //         return count;
 //     }
 // };
+
+// Sliding Window Approach
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        if(k <= 1) return 0;
+        int count = 0;
+        int n = nums.size();
+        int i = 0, j = 0;
+        int prod = 1;
+        while(j < n){
+            prod *= nums[j];
+            while(prod >= k){
+                count += (j-i);
+                prod /= nums[i];
+                i++;
+            }
+            j++;
+        }
+        while(i < n){
+            count += (j-i);
+            prod /= nums[i];
+            i++;
+        }
+        return count;
+    }
+};
