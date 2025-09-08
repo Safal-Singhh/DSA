5c16a026a714fdaf38d28ea3ff215a9adef586ef
Safal-Singhh <safalsingh999@gmail.com>
2025-09-09 00:01:47 +0530

    updated new file

--- Arrays/Stacks/239. Sliding Window Maximum.cpp
+++ Arrays/Stacks/239. Sliding Window Maximum.cpp
@@ -14,3 +14,35 @@
 //         return res;
 //     }
 // }; 
+
+// Optimal Approach using Stacks
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> res;
+        stack<int> st;
+        vector<int> ngi(n);
+        ngi[n-1] = n;
+        st.push(n-1);
+        for(int i = n-2; i >= 0; i--){
+            while(!st.empty() && nums[st.top()] <= nums[i]){
+                st.pop();
+            }
+            if(st.empty()) ngi[i] = n;
+            else ngi[i] = st.top();
+            st.push(i);
+        }
+        
+        for(int i = 0; i <= n-k; i++){
+            int mx = nums[i];
+            int j = i;
+            while(j < k+i){
+                mx = nums[j];
+                j = ngi[j];
+            }
+            res.push_back(mx);
+        }
+        return res;
+    }
+};
