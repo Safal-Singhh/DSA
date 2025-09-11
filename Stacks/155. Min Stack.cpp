affce4b19b28b5106489c1c49ef2ae655aeae579
Safal-Singhh <safalsingh999@gmail.com>
2025-09-12 01:55:43 +0530

    updated new file

--- Stacks/155. Min Stack.cpp
+++ Stacks/155. Min Stack.cpp
@@ -33,3 +33,36 @@
 //         return mn;
 //     }
 // };
+
+
+// Method - 2
+// class MinStack {
+// public:
+//     stack<int> st;
+//     stack<int> second;
+//     MinStack() {
+        
+//     }
+    
+//     void push(int val) {
+//         st.push(val);
+//         if(second.empty() || val < second.top()){
+//             second.push(val);
+//         } else{
+//             second.push(second.top());
+//         }
+//     }
+    
+//     void pop() {
+//         st.pop();
+//         second.pop();
+//     }
+    
+//     int top() {
+//         return st.top();
+//     }
+    
+//     int getMin() {
+//         return second.top();
+//     }
+// };
