a34f6c9437f44caa4118d1163cf2e36fb4d844df
Safal-Singhh <safalsingh999@gmail.com>
2025-09-12 03:11:09 +0530

    updated new file

--- Stacks/155. Min Stack.cpp
+++ Stacks/155. Min Stack.cpp
@@ -1,5 +1,4 @@
 // Brute Force Approach
-
 // class MinStack {
 // public:
 //     stack<int> st;
@@ -66,3 +65,53 @@
 //         return second.top();
 //     }
 // };
+
+// Optimal Approach
+class MinStack {
+public:
+    stack<long long> st;
+    long long min;
+    MinStack() {
+        min = LLONG_MAX;  
+    }
+    
+    void push(int val) {
+        long long x = (long long)val;
+        if(st.empty()){
+            st.push(x);
+            min = x;
+        }
+        else if(x >= min) st.push(x);
+        else{
+            st.push(2*x - min);
+            min = x;
+        }
+    }
+    
+    void pop() {
+        if(st.top() < min){ // Fake value present
+            // before popping make sure to retrieve the oldMin
+            long long oldMin = 2*min - st.top();
+            min = oldMin;
+        }
+        st.pop();
+    }
+    
+    int top() {
+        if(st.top() < min) return (int)min;
+        else return (int)st.top();
+    }
+    
+    int getMin() {
+        return (int)min;
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
