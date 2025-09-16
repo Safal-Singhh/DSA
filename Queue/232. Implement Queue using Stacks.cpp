e4d028a86a3d13e8dc4c1d96e71ad08084412c15
Safal-Singhh <safalsingh999@gmail.com>
2025-09-17 03:07:42 +0530

    updated new file

--- Queue/232. Implement Queue using Stacks.cpp
+++ Queue/232. Implement Queue using Stacks.cpp
@@ -1,3 +1,4 @@
+// Push efficient approach
 class MyQueue {
 public:
     stack<int> st1;
@@ -35,6 +36,50 @@ public:
             st2.pop();
         }
         return x;
+    }
+    
+    bool empty() {
+        if(st1.empty()) return true;
+        else return false;
+    }
+};
+
+
+// Pop efficient approach
+class MyQueue {
+public:
+    stack<int> st1;
+    stack<int> st2;
+    MyQueue() {
+        
+    } 
+    
+    void push(int x) {
+        if(st1.size() == 0){
+            st1.push(x);
+            return;
+        }
+        else{
+            while(!st1.empty()){
+                st2.push(st1.top());
+                st1.pop();
+            }
+            st1.push(x);
+            while(!st2.empty()){
+                st1.push(st2.top());
+                st2.pop();
+            }
+        }
+    }
+    
+    int pop() {
+        int x = st1.top();
+        st1.pop();
+        return x;
+    }
+    
+    int peek() {
+        return st1.top();
     }
     
     bool empty() {
